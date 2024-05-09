3d31d69a6bf8dd39a5559b0fa3a91dc38cbd4cf4
Andrey Luzin <andrey.luzin@workato.com>
2024-05-09 23:19:05 +0400

    Refactor benches

--- bwarr_bench_test.go
+++ bwarr_bench_test.go
@@ -12,39 +12,11 @@ import (
 const elemsOnStart = 4*1024*1024 - 1
 
 func BenchmarkQA_Insert(b *testing.B) {
-	bwa := New(int64Cmp, elemsOnStart)
-
-	for i := 0; i < elemsOnStart; i++ {
-		bwa.Insert(rand.Int63())
-	}
-	preparedData := make([]int64, b.N)
-	for i := 0; i < b.N; i++ {
-		preparedData[i] = rand.Int63() //nolint:gosec
-	}
-	b.SetBytes(8) //nolint:exhaustruct
-	b.ResetTimer()
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		bwa.Insert(preparedData[i])
-	}
+	benchmarkAppend(b, elemsOnStart, elemsOnStart)
 }
 
 func BenchmarkQA_ReplaceOrInsertNotFound(b *testing.B) {
-	bwa := New(int64Cmp, elemsOnStart)
-
-	for i := 0; i < elemsOnStart; i++ {
-		bwa.Insert(rand.Int63())
-	}
-	preparedData := make([]int64, b.N)
-	for i := 0; i < b.N; i++ {
-		preparedData[i] = rand.Int63() //nolint:gosec
-	}
-	b.SetBytes(8) //nolint:exhaustruct
-	b.ResetTimer()
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		bwa.ReplaceOrInsert(preparedData[i])
-	}
+	benchmarkReplace(b, elemsOnStart, elemsOnStart)
 }
 
 func BenchmarkQA_ReplaceOrInsertFound(b *testing.B) {
@@ -91,12 +63,100 @@ func BenchmarkQA_HasNotFoundWorst(b *testing.B) {
 	}
 	preparedData := make([]int64, b.N)
 	for i := 0; i < b.N; i++ {
-		preparedData[i] = rand.Int63() //nolint:gosec
+		preparedData[i] = rand.Int63()
 	}
 	b.SetBytes(8) //nolint:exhaustruct
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		bwa.Has(preparedData[i])
+	}
+}
+
+func BenchmarkQA_Min(b *testing.B) {
+	bwa := New(int64Cmp, elemsOnStart)
+
+	for range elemsOnStart {
+		bwa.Insert(rand.Int63())
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		bwa.Min()
+	}
+}
+
+func BenchmarkQA_Delete(b *testing.B) {
+	bwa := New(int64Cmp, elemsOnStart+b.N)
+	for range elemsOnStart {
+		bwa.Insert(rand.Int63())
+	}
+
+	toDel := make([]int64, b.N)
+	for i := range b.N {
+		toDel[i] = rand.Int63()
+		bwa.Insert(toDel[i])
+	}
+	rand.Shuffle(len(toDel), func(i, j int) { toDel[i], toDel[j] = toDel[j], toDel[i] })
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		if _, found := bwa.Delete(toDel[i]); !found {
+			b.Fail()
+		}
+	}
+}
+
+func BenchmarkQA_DeleteMin(b *testing.B) {
+	const elemsOnStart = 4 * 1024 * 1024
+	elems := elemsOnStart + b.N
+	bwa := New(int64Cmp, elems)
+
+	for range elems {
+		bwa.Insert(rand.Int63())
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		bwa.DeleteMin()
+	}
+}
+
+func benchmarkAppend(b *testing.B, elemsOnStart, capacity int) {
+	bwa := New(int64Cmp, capacity)
+
+	for i := 0; i < elemsOnStart; i++ {
+		bwa.Insert(rand.Int63())
+	}
+	preparedData := make([]int64, b.N)
+	for i := 0; i < b.N; i++ {
+		preparedData[i] = rand.Int63()
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		bwa.Insert(preparedData[i])
+	}
+}
+
+func benchmarkReplace(b *testing.B, elemsOnStart, capacity int) {
+	bwa := New(int64Cmp, capacity)
+
+	for i := 0; i < elemsOnStart; i++ {
+		bwa.Insert(rand.Int63())
+	}
+	preparedData := make([]int64, b.N)
+	for i := 0; i < b.N; i++ {
+		preparedData[i] = rand.Int63()
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		bwa.ReplaceOrInsert(preparedData[i])
 	}
 }
